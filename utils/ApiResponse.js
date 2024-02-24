9a2912782378ab4f391251294943c4c4c8302320
Krishna Bhagat <krishnakumar329865@gmail.com>
2024-02-24 16:15:11 +0530

    Added some more files for backend

--- utils/ApiResponse.js
+++ utils/ApiResponse.js
@@ -11,3 +11,13 @@ class ApiResponse {
 
 export {ApiResponse}
 
+// The Error class is built-in, but here’s its approximate code so we can understand what we’re extending:
+
+// The "pseudocode" for the built-in Error class defined by JavaScript itself
+// class Error {
+//   constructor(message) {
+//     this.message = message;
+//     this.name = "Error"; // (different names for different built-in error classes)
+//     this.stack = <call stack>; // non-standard, but most environments support it
+//   }
+// }
